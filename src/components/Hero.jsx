43a2390c1cb09f584b58aff86925417499523dc2
Laura Rivas <diana.laura.rivas00@hotmail.com>
2024-10-24 20:52:39 -0600

    Modificacion de botones

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { VscArrowSmallRight } from "react-icons/vsc";
 
 const Hero = () => {
   return (
     <section className='h-[90vh] grid grid-cols-1 md:grid-cols-8'> 
     <div className='md:col-span-5 flex items-center justify-center p-8'>
-      <div>
+      <div className='flex flex-col gap-3'>
         <h1 className='text-7xl font-bold leading-[9rem]'> 
           
           Conecta con  <span className='text-primary px-2 border-8 border-primary'> Student Connect </span>
@@ -19,10 +20,12 @@ const Hero = () => {
           comunidad donde cada viaje mejora tu experiencia diaria.
           </p>
           <div>
-            <button className='bg-primary text-white px-6 rounded-xl text-xl'> Conocenos </button>
+            <button className='flex items-center gap-4 bg-primary text-white py-2 px-8 rounded-xl text-xl'> 
+             Regístrate <VscArrowSmallRight /> </button>
+            <button className='py-2 px-8 rounded-xl text-xl'> Conócenos </button>
           </div>
       </div>
-    </div>
+    </div>  
       <div className='md:col-span-3'> 
 
       </div>
