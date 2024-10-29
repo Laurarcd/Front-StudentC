852b124b7fcebbd1d160043ef9c1d323ba2a7cca
Laura Rivas <diana.laura.rivas00@hotmail.com>
2024-10-28 20:12:01 -0600

    LandingComplete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { VscArrowSmallRight } from "react-icons/vsc";
 
 const Hero = () => {
   return (
-    <section className='min-h-[90vh] grid grid-cols-1 md:grid-cols-8'> 
+    <section className='min-h-[90vh] grid grid-cols-1 xl:grid-cols-8'> 
     <div className='md:col-span-5 flex items-center justify-center p-8 xl:p-14'>
       <div className='flex flex-col gap-3'>
         <h1 className='text-7xl font-bold leading-[9rem]'> 
@@ -20,12 +20,12 @@ const Hero = () => {
           comunidad donde cada viaje mejora tu experiencia diaria.
           </p>
           <div className='flex flex-col md:flex-row items-center gap-8'>
-            <button className='flex items-center gap-4 bg-primary text-white py-2 px-8 rounded-xl text-xl'> 
-             Regístrate <VscArrowSmalrfrggrelRight /> </button>
-            <button className='w-full items-center bg-gray-600 text-white py-2 px-12 rounded-xl text-xl '> Conócenos </button>
+            <button className='w-full xl:w-auto flex items-center bg-primary text-white justify-start text-left gap-4 -py-4 px-4 rounded-xl text-xl'> 
+             Regístrate <VscArrowSmallRight className='p-4 rounded-full box-content'/>  </button>
+            <button className='w-full xl:w-auto items-center bg-gray-600 text-white py-3 px-12 rounded-xl text-xl '> Conócenos </button>
           </div>
       </div>
-    </div>  
+    </div>    
       <div className='md:col-span-3 flex items-center justify-center'> 
         <img src='Mapa.png' className='object-cover'/>
       </div>
