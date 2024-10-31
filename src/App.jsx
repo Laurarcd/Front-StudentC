e6f233775f90216fbc37625c05fa806892e1c317
Laura Rivas <diana.laura.rivas00@hotmail.com>
2024-10-30 19:27:55 -0600

    Se agrego About

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import About from "./components/About"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <div> 
         <Header/>
         <Hero/>
+        <About/>
       </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const About = () => {
+  return (
+
+            <div className="container mx-auto px-4">
+                <div className="flex flex-wrap">
+                    <div className="w-full md:w-1/2">
+                        <img src=""/>
+                    </div>
+                    <div className="w-full md:w-1/2">
+                        <div className="px-4">
+                            <h2 className="text-2xl text-primary font-bold mt-0 mb-2">¿Cómo funciona Student Connect? </h2>
+                            <h3 className="text-xl font-bold mt-4 mb-2">Why Choose Us?</h3>
+                            <div className="list-disc space-y-2 pl-5">
+                                <div className="text-base">Lorem ipsum dolor sit amet</div>
+                                <div className="text-base">Tempor incididunt ut labore</div>
+                                <div className="text-base">Lorem ipsum dolor sit amet</div>
+                                <div className="text-base">Incididunt ut labore</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+export default About;
